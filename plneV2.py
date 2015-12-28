6237c8a64840c08148d7929e35b32923136fcfcf
Valerie Daras <daras@etud.insa-toulouse.fr>
2015-12-28 22:21:17 +0100

    PLNE V1 et V2 Fonctionnel!

--- plneV2.py
+++ plneV2.py
@@ -2,102 +2,84 @@
 """
 Created on Mon Dec  7 08:21:03 2015
 
-@author: valeriedaras
+@author: Valerie Daras et Julie Riviere
 """
 
-''' Notations :
-Graphe G=(V,E)
-Identification d'une partition :    variable xi par sommet i
-                                    xi = 1 si xi est représentant d'une partition
-                                    xi = 0 sinon
-                                    
-Identification de deux sommets i et j :
-                                    xij = 1 si i et j dans la même partition
-                                    xij = 0 sinon
-                                    
-Poids entre deux sommets i et j : cij
-                                    
-Identification d'arêtes entre deux sommets i et j :
-                ensemble Ec = {(i,j) tel que (i,j) appartenant à E et xij = 0, i!=j}
-                variable Aij = 1 si xij = 0 et (i,j) appartient à E
-                         Aij = 0 sinon
-                         
-                         si l'arrete n'existe pas => Aij initialisé à 0
-                         
-Fonction objectif : 
-                min (sum sur i de 1 à n-1, sum sur j de i+1 à n, (cij x Aij), i<j)
-                
-                
-Contraintes : 
-1. Unicité d'un représentant dans une partition:
-    Pour un sommet i, sum sur j de 1 à n, j!=i, (xi x xj x Aij = 0)
-2. Nombre de parties : k = sum sur i de 1 à n, (xi)
-3. Inégalité triangulaire : xij + xik -1 <= xjk 
-4. Choix d'un représentant : pour tout les sommets i tels que xi=1
-                            sum sur j de 1 à i-1 (xi x xij) = 0
-                
-'''
-import sys
 from gurobipy import *
 import script as s
 import objectivefunctions as objf
 
+
+# Fonction permettant de calculer la représentation matricielle d'un graphe
+# @graph : graphe à étudier
+# Return : matrice cij
 def weightMatrix(graph):
-    cij = [[0 for x in range (graph.number_of_nodes())] for x in range (graph.number_of_nodes())]  
-    for i in range (graph.number_of_nodes()):
-        for j in range (graph.number_of_nodes()):
+    n = graph.number_of_nodes()
+    cij = [[0 for x in range (n)] for x in range (n)]
+    for i in range (n):
+        for j in range (i+1, n):
             cij[i][j] = objf.calculateWeight(i+1,j+1,graph)
     return cij
-    
 
+
+# Fonction permettant de définir les variables du problème et la fonction objectif
+# @cij : représentation matricielle du graphe
+# @graph : graphe à étudier
 def defineObjf(cij, graph):
     n = graph.number_of_nodes()
     
-    #X : Variable Xi
+    # Variable Xi
     global X
     X = {}
     
-    #Y : Variable Xij
+    #Y : Variable Yij
     global Y
     Y = {}
-    
+        
     #Z : Variable Zij
     global Z 
     Z = {}
     
-    # Variable model Gurobi
+    # Variable modèle Gurobi
     global model 
     model = Model('partitioning')
     
-    for i in range(1,n+1):
+    # Ajout de la variable Xi au modèle
+    for i in range(1, n+1):
         X[i] = model.addVar(vtype=GRB.BINARY, name="X_"+str(i), obj=0)
-    
-    for i in range(1,n):
-        for j in range(i+1,n+1):
-            Y[i,j] = model.addVar(vtype=GRB.CONTINUOUS, name="Y"+str(i)+"_"+str(j), obj=-cij[i-1][j-1])
+
+    # Ajout de la variable Yij au modèle    
+    for i in range(1, n):
+        for j in range(i+1, n+1):
+            Y[i,j] = model.addVar(vtype=GRB.BINARY, name="Y_"+str(i)+"_"+str(j), obj=-cij[i-1][j-1])
             Y[j,i] = Y[i,j]
 
+    # Ajout de la variable Zij au modèle
     for j in range(1,n+1):
         for i in range(1,j):
             Z[i,j] = model.addVar(vtype=GRB.BINARY, name="Z_"+str(i)+"_"+str(j), obj=0)
-            
+
+    # Problème de minimisation
     model.modelSense = GRB.MINIMIZE
     model.update()
+    
+ 
 
 
+
+# Fonction permettant de définir les contraintes du problème  
+# @cij : représentation matricielle du graphe
+# @graph : graphe à étudier
+# @k : nombre de partitions à créer
 def defineConstraints(cij, graph, k):
+    # Nombre de noeuds dans le graphe
     n = graph.number_of_nodes()
     
+    # Contrainte n°1 :
     # Le nombre de partitions est égal au nombre de représentants
     model.addConstr(quicksum(X[i] for i in range (1,n+1)) == k)
-
-    # Relaxation linéraire sur les Yij
-    for i in range(1,n+1):   
-        for j in range (1,i):
-            model.addConstr(Y[i,j], "<=", 1)
-            model.addConstr(Y[i,j], ">=", 0)
-
-    # Inégalité triangulaire 
+    
+    # Contrainte n°2 : Inégalité triangulaire 
     # Si i est dans une partition avec j et k, 
     # alors j et k sont aussi dans une même partition
     for i in range(1,n+1):
@@ -109,55 +91,63 @@ def defineConstraints(cij, graph, k):
                         L3a.addConstant(-1)
                         L3b = LinExpr([1],[Y[j,k]])
                         model.addConstr(L3a, "<=", L3b)
-    '''
-    V1 : 
+        
+    # Contrainte n°3 : Choix des représentants
     # Un sommet i connait soit des voisins déjà représentants, 
     # soit il devient représentant
-    for i in range(1,n+1):
-        model.addConstr(X[i] + quicksum(Y[i,j] for j in range (1,i)), ">=", 1)
-        # Un sommet i représentant d'une partition ne peut pas avoir de sommet j voisin en-dessous de lui      
-        for j in range (1,i):
-            L1 = LinExpr(X[i]+Y[i,j])
-            model.addConstr(L1, "<=", 1)
-    '''
-    #V2 : Linéarisation des contraintes concernant le représentant d'une partition
+    # Linéarisation des contraintes concernant le représentant d'une partition
     for j in range(1,n+1):
         model.addConstr(X[j] + quicksum(Z[i,j] for i in range (1,j) ) == 1)
-    
+
     for j in range(1,n+1):
         for i in range(1,j):
             model.addConstr(Z[i,j], "<=", X[i])
             model.addConstr(Z[i,j], "<=", Y[i,j])
             model.addConstr(Z[i,j], ">=", X[i] + Y[i,j] -1)
     
-    model.update()
+    # Relaxation linéaire sur les Yij
+    for i in range(1,n+1):   
+        for j in range (1,i):
+            model.addConstr(Y[i,j], "<=", 1)
+            model.addConstr(Y[i,j], ">=", 0)
 
-def plne(graph, k):
+
+
+# Fonction permettant d'initialiser le problème et de lancer sa résolution
+# @graph : graphe à étudier
+# @k : nombre de partitions 
+def plne(graph,k):
+    
+    # Calcul de la représentation matricielle du graphe
     cij = weightMatrix(graph)
-    defineObjf(cij, graph)
+
+    # Définition des variables et de la fonction objectif
+    defineObjf(cij, graph) 
+
+    # Définition des contraintes
     defineConstraints(cij,graph,k)
+
+    # Lancement du solver
     model.optimize()
-    print "X[i]:", X
-    '''
-    for i in range(graph.number_of_nodes()):
-        if Y[1,i] == 1:
-            print i'''
+    
+    # Vérification du status du solver / de la solution
     s = model.status
     if s == GRB.Status.UNBOUNDED:
         print "Model cannot be solved because it is unbounded"
-        #exit(0)
     if s == GRB.Status.OPTIMAL:
         print "The optimal objective is %g" % model.objVal
-        #exit(0)
-    if s == GRB.Status.INF_OR_UNBD and status != GRB.Status.INFEASIBLE:
+        n = graph.number_of_nodes()
+        # Affichage de la solution
+        displayPartitions(n,k)
+    if s == GRB.Status.INF_OR_UNBD and s != GRB.Status.INFEASIBLE:
         print "Optimization was stopped with status %d" % s
-        #exit(0) 
+
 
 def main():
-    #copyFilename = "/Users/User/Documents/GitHub/GraphPartitioning/unitEx.graph"
-    copyFilename = "/Users/valeriedaras/Documents/INSA/5IL/DataMining/workspace/GraphPartitioning/unitEx.graph"
+    copyFilename = "unitEx.graph"
     graph = s.createGraph(copyFilename)
     plne(graph, 2)
     
+
 if __name__ == '__main__':
     main()
