699567907b7df3666be355f4d87caeac47551bab
Julie-R <riviere.julie@hotmail.com>
2015-12-30 17:48:19 +0100

    gloutonWithCut added

--- glouton.py
+++ glouton.py
@@ -6,7 +6,7 @@ Created on Fri Nov 27 15:44:09 2015
 """
 import sys 
 import operator
-
+import objectivefunctions as objf
 
 # a supprimer : pour les tests 
 import script as s
@@ -73,7 +73,9 @@ def glouton(k, graph):
     # initialisation du sommet de départ
     s0 = nodeWithLessNeighbor(graph, markingList)
     markingList.append(s0)
+    print "sommet de depart :", s0
     potentialNodesList = setPotentialNodesList(graph,s0, markingList, potentialNodesList)
+    print "potentialNodesList : ", potentialNodesList
     # initialisation de i : numéro de partition courante
     i = 1
     while i < k:
@@ -107,13 +109,70 @@ def glouton(k, graph):
     # probleme : si on ne return pas le graphe, ici il est bien égal
     # à tout le graphe mais sans le return il vaut une partition...
     return partitionList, graph
+    
+    
+     # parametres : k le nombre de partitions, graphe    
+def gloutonWithCut(k, graph):
+    # initialisation
+    initGraph = graph.copy()
+    niList = []
+    markingList = [] 
+    potentialNodesList = []
+    partitionList = []
+    # initialisation du sommet de départ
+    s0 = nodeWithLessNeighbor(graph, markingList)
+    markingList.append(s0)
+    print "sommet de depart :", s0
+    potentialNodesList = setPotentialNodesList(graph,s0, markingList, potentialNodesList)
+    # initialisation de i : numéro de partition courante
+    i = 1
+    while i < k:
+        # Calcul de ni : nombre de sommets pour la prochaine partition
+        niList = calculateSizeListSubGraph(k, i, niList, initGraph)
+        ni = niList[i-1]
+        #print ni
+        # j : nombre de sommets actuellement dans la partition courante
+        j=1 	# on compte le sommet de départ
+        while potentialNodesList != [] and j < ni:
+            minCut = sys.maxint
+            for node in potentialNodesList:
+                testList = list(markingList)
+                testList.append(node[0])
+                remainingNodes = [x for x in graph.nodes() if x not in testList]
+                if objf.calculateCut(testList,remainingNodes,graph) < minCut:
+                    minCut = objf.calculateCut(testList,remainingNodes,graph)
+                    minNode = node[0]
+            print "prochain noeud ajouté dans la partition :", minNode
+            markingList.append(minNode)
+            potentialNodesList = setPotentialNodesList(graph,minNode, markingList, potentialNodesList)
+            j = j+1
+        # Affectation de la partition créée à la liste de partitions
+        partitionList.append(markingList)
+        # Réinitialisation des structures
+        # Restructuration du graphe : suppression des sommets marqués
+        graph.remove_nodes_from(markingList)
+        markingList = [] 
+        potentialNodesList = []
+        i = i+1
+        # Initialisation de la prochaine partition
+        if i != k :
+            s0 = nodeWithLessNeighbor(graph, markingList)
+            markingList.append(s0)
+            potentialNodesList = setPotentialNodesList(graph,s0, markingList, potentialNodesList)
+    # partitionList ← sommets restants 
+    partitionList.append(graph.nodes())
+    graph = initGraph
+    # probleme : si on ne return pas le graphe, ici il est bien égal
+    # à tout le graphe mais sans le return il vaut une partition...
+    return partitionList, graph
 
         
 def main():
     copyFilename = "unitEx.graph"
     graph = s.createGraph(copyFilename)
-    pList, graph = glouton(3,graph)
+    pList, graph = gloutonWithCut(2,graph)
     print pList
+    print objf.calculateCut(pList[0],pList[1],graph)
 
 def testremoveMarkingNodes():
     theList = [(1, {'weight': 1}), (2, {'weight': 1}), (1, {'weight': 8}),(1, {'weight': 8})]
@@ -121,6 +180,5 @@ def testremoveMarkingNodes():
     theList = removeMarkingNodes(theList, markingList)
     print theList
     
-
 if __name__ == '__main__':
         main()
